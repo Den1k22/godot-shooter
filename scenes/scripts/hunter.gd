602343a60256ca492a191ced70182ad62cf9c24e
Den Kom <deniss.komarskis@gmail.com>
2024-09-16 17:25:37 +0300

    impr: created hunter with bone animation

--- scenes/scripts/hunter.gd
+++ scenes/scripts/hunter.gd
@@ -2,8 +2,12 @@ extends CharacterBody2D
 
 var active: bool = false
 var speed: int = 200
+var can_attack: bool = false
+var health: int = 100
 
 func _ready() -> void:
+	$NavigationAgent2D.path_desired_distance = 4.0
+	$NavigationAgent2D.target_desired_distance = 4.0
 	$NavigationAgent2D.target_position = Globals.player_pos
 
 	
@@ -13,17 +17,37 @@ func _physics_process(_delta: float) -> void:
 		var direction: Vector2 = (next_path_pos - global_position).normalized()
 		velocity = direction * speed
 		move_and_slide()
-		look_at(Globals.player_pos)
+		var look_angle = direction.angle()
+		rotation = look_angle + PI / 2
+
+func hit():
+	health -= 10
+	if health <= 0:
+		queue_free()
 
 
 func _on_notice_area_body_entered(_body: Node2D) -> void:
 	active = true
+	$AnimationPlayer.play("walk")
 
 
 func _on_notice_area_body_exited(_body: Node2D) -> void:
 	active = false
 
-
 func _on_navigation_timer_timeout() -> void:
 	if active:
 		$NavigationAgent2D.target_position = Globals.player_pos
+
+
+func _on_attack_area_body_entered(_body: Node2D) -> void:
+	can_attack = true
+	$AnimationPlayer.play("attack")
+
+
+func _on_attack_area_body_exited(_body: Node2D) -> void:
+	can_attack = false
+
+
+func attack():
+	if can_attack:
+		Globals.health -= 20
